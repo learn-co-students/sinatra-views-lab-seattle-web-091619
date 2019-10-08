c436735525f39d263e748407eecd230764e827ae
Garrett Burns <GBmixo>
2019-10-08 11:26:14 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,17 @@ class App < Sinatra::Base
 
 	get '/' do
 		erb :index
-	end
+        end
+        get '/hello' do
+                erb :hello
+        end
+        get '/goodbye' do
+                erb :goodbye
+        end
+        get '/date' do
+                erb :date
+        end
+
 
 
 end
